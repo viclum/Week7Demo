1a1fe8ab6a5580bfb345a7ebf2d4061fd95e4cbb
Victor Lum <viclum@zoho.com>
2022-11-29 13:19:01 +0800

    Correct type and add change radius

--- Week7Demo/Program.cs
+++ Week7Demo/Program.cs
@@ -12,6 +12,15 @@ void InitCircleList(List<Circle> cList)
     cList.Add(circle3);
 }
 
+void DisplayList(List<Circle> circleList)
+{
+    int count = 1;
+    foreach (var obj in circleList)
+    {
+        Console.WriteLine("[{0}] Type: {1,-8} Color: {2,-8} Radius: {3}", count, "Circle", obj.Color, obj.Radius);
+        count += 1;
+    }
+}
 List<Circle> circleList = new List<Circle>();
 InitCircleList(circleList);
 circleList.Sort();
@@ -21,3 +30,17 @@ for (int i = 0; i < circleList.Count; i++)
     Circle c = circleList[i];
     Console.WriteLine("{0, -4} {1, -10} {2, -10} {3}", (i + 1), c.Type, c.Color, c.Radius);
 }
+DisplayList(circleList);
+Console.Write("Enter Circle Number: ");
+int Cnumber = Convert.ToInt32(Console.ReadLine());
+if ((Cnumber >= 1) && (Cnumber <= circleList.Count))
+{
+    Console.Write("Enter new radius: ");
+    int newRadius = Convert.ToInt32(Console.ReadLine());
+    circleList[Cnumber - 1].Radius = newRadius;
+}
+else
+{
+    Console.WriteLine("That doesn't exist.");
+}
+DisplayList(circleList);
